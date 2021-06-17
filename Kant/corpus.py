6e366a03f01a12cf3d3faf3edb2b6ba9ed862e30
Akoua_Orsot <60398489+aorsot1@users.noreply.github.com>
2021-06-17 08:22:57 -0400

    Create corpus.py

---
+++ Kant/corpus.py
@@ -0,0 +1,21 @@
+# Libraries
+import numpy as np
+import pandas as pd
+
+import math
+import os
+import re
+import string
+import random
+import urllib3.request
+import zipfile
+import requests
+
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+from nltk.tokenize import RegexpTokenizer
+import matplotlib.pyplot as plt
+
+import warnings
+warnings.filterwarnings("ignore")
